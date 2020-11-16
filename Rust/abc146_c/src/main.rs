d554abe095d7d964a03671d58b8b8a4f7ac8ed3e
MURAKI Taichi <muraki.taichi@gmail.com>
2020-11-16 09:33:45 +0900

    fix

--- Rust/abc146_c/src/main.rs
+++ Rust/abc146_c/src/main.rs
@@ -1,3 +1,22 @@
+/// # 問題
+/// 2020/07/28
+/// https://atcoder.jp/contests/abc146/tasks/abc146_c
+///
+/// # 提出
+/// https://atcoder.jp/contests/abc161/submissions/18074812
+use std::cmp;
+
+fn read() -> String {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    s.trim_end().to_owned()
+}
+
+fn to_number_vector(s: &str) -> Vec<i64> {
+    let numbers = s.split_whitespace().map(|s| s.parse().unwrap()).collect();
+    numbers
+}
+
 fn main() {
     println!("Hello, world!");
 }

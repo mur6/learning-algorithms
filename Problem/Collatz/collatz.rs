d8fbe1f32fb7c8ee53b27a492f71fe9e525f2f0f
MURAKI Taichi <muraki.taichi@gmail.com>
2021-01-15 15:34:12 +0900

    Fix bug in rust version.

--- Problem/Collatz/collatz.rs
+++ Problem/Collatz/collatz.rs
@@ -4,13 +4,16 @@ fn collatz(n: u64) -> Vec<u64> {
     assert!(n > 0);
     let mut vec = Vec::new();
     let mut n = n;
-    while n != 1 {
+    loop {
+        vec.push(n);
+        if n == 1 {
+            break;
+        }
         if n % 2 == 0 {
             n = n / 2;
         } else {
             n = n * 3 + 1;
         }
-        vec.push(n);
     }
     vec
 }
@@ -18,6 +21,8 @@ fn collatz(n: u64) -> Vec<u64> {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input = args.get(1).unwrap();
-    let n: u64 =input.parse().unwrap();
-    println!("{:?}", collatz(n));
+    let n:u64 = input.parse().unwrap();
+    let answers: Vec<String> = collatz(n).into_iter().map(|i| i.to_string()).collect();
+    let answer = answers.join(" ");
+    println!("{}", answer);
 }
